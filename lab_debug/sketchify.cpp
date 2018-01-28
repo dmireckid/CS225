220bc475f8df31b0ffe244630facc7fe660b7295
Damian Mirecki <mirecki2@linux-a3.ews.illinois.edu>
2018-01-28 17:00:20 -0600

    debug done

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -13,43 +13,42 @@ PNG* setupOutput(unsigned w, unsigned h) {
 
 // Returns my favorite color
 HSLAPixel* myFavoriteColor(double saturation) {
-    HSLAPixel pixel(115, saturation, 0.5);
-    return &pixel;
+    HSLAPixel* pixel = new(HSLAPixel);
+    pixel->h=115;
+    pixel->s=saturation;
+    pixel->l=0.5;
+    return pixel;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-    cout << "Reached line 23" << endl;
-    original=readFromFile(inputFile);
+    PNG* original = new PNG();
+    original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
-    cout << "Reached line 27" << endl;
     // Create out.png
     PNG* output;
-    setupOutput(width, height);
+    output=setupOutput(width, height);
 
     // Load our favorite color to color the outline
     HSLAPixel* myPixel = myFavoriteColor(0.5);
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned int y = 1; y < height; y++) {
+        for (unsigned int x = 1; x < width; x++) {
             // Calculate the pixel difference
-            HSLAPixel & prev = original->getPixel(x - 1, y - 1);
-            HSLAPixel & curr = original->getPixel(x, y);
-            double diff = std::fabs(curr.h - prev.h);
-
+            HSLAPixel* prev = &original->getPixel(x - 1, y - 1);
+            HSLAPixel* curr = &original->getPixel(x, y);
+            double diff = std::fabs(curr->h - prev->h);
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel * currOutPixel = &(output->getPixel(x, y));
+            HSLAPixel * currOutPixel = &output->getPixel(x, y);
             if (diff > 20) {
-                currOutPixel = myPixel;
+                *currOutPixel = *myPixel;
             }
         }
     }
-
     // Save the output file
     output->writeToFile(outputFile);
 
