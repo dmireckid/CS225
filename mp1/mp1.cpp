dfb877efa2b74c27d614d8e064ccbce48aecc238
Damian Mirecki <mirecki2@linux-a2.ews.illinois.edu>
2018-01-29 23:42:21 -0600

    mp1 progress

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -6,12 +6,55 @@ using namespace cs225;
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // Part 2
+  PNG * in = new PNG();
+  in->readFromFile(inputFile);
+  unsigned int w = in->width();
+  unsigned int h = in->height();
+  PNG * out = new PNG(w, h);
+
+  for(unsigned int i=0; i<w; i++){
+    for(unsigned int j=0; j<h; j++){
+      HSLAPixel* ip = in->getPixel(i, j);
+      HSLAPixel* ipr = in->getPixel(w-i-1, h-j-1);
+      HSLAPixel* op = out->getPixel(w-i-1, h-j-1);
+      HSLAPixel* opr = out->getPixel(i, j);
+      *op = *ip;
+      *opr = *ipr;
+    }
+  }
+  out->writeToFile(outputFile);
+
+  delete out;
+  delete in;
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
-  // Part 3
+  /*for(unsigned int x = 0; x < (width/3); x++){
+    for(unsigned int y = 0; y < (height); y++){
+      png[x, y]->h=0;
+      png[x, y]->s=1;
+      png[x, y]->l=0.5;
+      png[x, y]->a=1;
+    }
+  }
+  for(unsigned int x = (width/3); x < (2*width/3); x++){
+    for(unsigned int y = 0; y < (height); y++){
+      png[x, y]->h=120;
+      png[x, y]->s=1;
+      png[x, y]->l=0.5;
+      png[x, y]->a=1;
+    }
+  }
+  for(unsigned int x = (2*width); x < width; x++){
+    for(unsigned int y = 0; y < (height); y++){
+      png[x, y]->h=240;
+      png[x, y]->s=1;
+      png[x, y]->l=0.5;
+      png[x, y]->a=1;
+    }
+  }*/
+
 
   return png;
 }
